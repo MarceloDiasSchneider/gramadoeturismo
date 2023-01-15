675e23c831c686380954a5fd1fb7005f5eef65c3
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2023-01-15 17:08:58 +0100

    create php code to send the message

---
+++ send_message.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// get the post parameters name, email e message
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$message .= "de: " . $name . " email: " . $email;
+
+// check of all parameters are set
+if (isset($name) && isset($email) && isset($message)) {
+  // send the email
+  $message = $message;
+  $subject = "Contato de Gramado e Turismo";
+  $email = "marcelo.d.schneider@gmail.com";
+  mail($email, $subject, $message);
+
+  echo json_encode(array(
+    "status" => "success",
+    "message" => "Email enviado com sucesso!"
+  ));
+} else {
+  // set the response code to (400 - Bad Request)
+  http_response_code(400);
+  // if not, return an error
+  echo json_encode(array(
+    "status" => "error",
+    "message" => "Erro ao enviar email!"
+  ));
+}
